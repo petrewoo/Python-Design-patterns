1bb732f42e0b23ac75ba5d8680da016216fe9f3b
Alex <petrewoo@hotmail.com>
2017-05-26 13:59:59 +0800

    Add factory method sample.

---
+++ factory_pattern/factory_method.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from .pizza import *
+from .pizzz_store import *
+
+
+class ChicagoPizzaStore(AbsPizzaStore):
+
+    def _create(self, pizza_type):
+        if pizza_type == 'cheese':
+            return ChicagoStyleCheesePizza()
+        elif pizza_type == 'pepperoni':
+            return ChicagoStylePepperoniPizza()
+        elif pizza_type == 'clam':
+            return ChicagoStyleClamPizza()
+        elif pizza_type == 'veggie':
+            return ChicagoStyleVeggiePizza()
+
+
+class NYPizzaStore(AbsPizzaStore):
+
+    def _create(self, pizza_type):
+        if pizza_type == 'cheese':
+            return NYStyleCheesePizza()
+        elif pizza_type == 'pepperoni':
+            return NYStylePepperoniPizza()
+        elif pizza_type == 'clam':
+            return NYStyleClamPizza()
+        elif pizza_type == 'veggie':
+            return NYStyleVeggiePizza()
+
+
+class CaliforniaPizzaStore(AbsPizzaStore):
+
+    def _create(self, pizza_type):
+        if pizza_type == 'cheese':
+            return CaliforniaStyleCheesePizza()
+        elif pizza_type == 'pepperoni':
+            return CaliforniaStylePepperoniPizza()
+        elif pizza_type == 'clam':
+            return CaliforniaStyleClamPizza()
+        elif pizza_type == 'veggie':
+            return CaliforniaStyleVeggiePizza()
+
+
+if __name__ == '__main__':
+    cps = ChicagoPizzaStore()
+    cps.order_pizza('cheese')
+    cps.order_pizza('pepperoni')
+
+    nyps = NYPizzaStore()
+    nyps.order_pizza('cheese')
+    nyps.order_pizza('pepperoni')
+
+    caps = CaliforniaPizzaStore()
+    caps.order_pizza('cheese')
+    caps.order_pizza('pepperoni')
--- factory_pattern/pizza_store.py
+++ factory_pattern/pizza_store.py
@@ -30,58 +30,7 @@ class SimplePizzaStore(AbsPizzaStore):
             return VeggiePizza()
 
 
-class ChicagoPizzaStore(AbsPizzaStore):
-
-    def _create(self, pizza_type):
-        if pizza_type == 'cheese':
-            return ChicagoStyleCheesePizza()
-        elif pizza_type == 'pepperoni':
-            return ChicagoStylePepperoniPizza()
-        elif pizza_type == 'clam':
-            return ChicagoStyleClamPizza()
-        elif pizza_type == 'veggie':
-            return ChicagoStyleVeggiePizza()
-
-
-class NYPizzaStore(AbsPizzaStore):
-
-    def _create(self, pizza_type):
-        if pizza_type == 'cheese':
-            return NYStyleCheesePizza()
-        elif pizza_type == 'pepperoni':
-            return NYStylePepperoniPizza()
-        elif pizza_type == 'clam':
-            return NYStyleClamPizza()
-        elif pizza_type == 'veggie':
-            return NYStyleVeggiePizza()
-
-
-class CaliforniaPizzaStore(AbsPizzaStore):
-
-    def _create(self, pizza_type):
-        if pizza_type == 'cheese':
-            return CaliforniaStyleCheesePizza()
-        elif pizza_type == 'pepperoni':
-            return CaliforniaStylePepperoniPizza()
-        elif pizza_type == 'clam':
-            return CaliforniaStyleClamPizza()
-        elif pizza_type == 'veggie':
-            return CaliforniaStyleVeggiePizza()
-
-
 if __name__ == '__main__':
     sps = SimplePizzaStore()
     sps.order_pizza('cheese')
     sps.order_pizza('pepperoni')
-
-    cps = ChicagoPizzaStore()
-    cps.order_pizza('cheese')
-    cps.order_pizza('pepperoni')
-
-    nyps = NYPizzaStore()
-    nyps.order_pizza('cheese')
-    nyps.order_pizza('pepperoni')
-
-    caps = CaliforniaPizzaStore()
-    caps.order_pizza('cheese')
-    caps.order_pizza('pepperoni')
